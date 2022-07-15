0b24fdc95e2601f3b2bd6222bc8a60e2395a2272
nereazam <nerea_zama@hotmail.com>
2022-07-15 19:26:08 +0200

    add api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-ejercicio!
+
+Se trata de una aplicación web de búsqueda de series de anime, que nos permite des/marcar las series como favoritas y guardarlas en local storage.
+El ejercicio tiene una parte de maquetación con HTML y Sass, y una parte de JavaScript.
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,47 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+//method: "POST",
+//headers: {"content-Type": "application/json" },
+// body: JSON.stringify(data),
+
+// SECCIÓN 1. ELEMENTOS DEL HTML
+const input = document.querySelector(".js-input");
+const reset = document.querySelector(".js-reset");
+const search = document.querySelector(".js-search");
+const listUl = document.querySelector(".js-list");
+
+// SECCIÓN 2. VARIABLES GLOBALES
+let result = [];
+
+// SECCIÓN 3. FUNCIONES
+function renderFilms() {
+  let html = "";
+
+  for (const film of result) {
+    html += `<li>${film.tittle}</li>`;
+    html += `<li>${film.images}</li>`;
+
+    for (const image of film.images) {
+      console.log(film.images);
+    }
+  }
+
+  listUl.innerHTML = html;
+}
+// SECCIÓN 4. FUNCIONES DE EVENTOS
+
+// SECCIÓN 5. EVENTOS
+
+search.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const userElection = input.value;
+
+  fetch(`https://api.jikan.moe/v4/anime?q=${userElection}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      result = json.data;
+      renderFilms();
+    });
+});
