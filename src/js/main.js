251874c52c7ca9c790c3cfa7e9f43c733870f15a
nereazam <nerea_zama@hotmail.com>
2022-07-16 18:07:33 +0200

    add placeholder

--- src/js/main.js
+++ src/js/main.js
@@ -11,16 +11,27 @@ let result = [];
 
 function renderFilms() {
   let html = "";
+  
+  
+  
   for (const anime of result) {
-    html += `<article class= "list js-list-anime" id="${anime.mal_id}">`;
-    html += `<img src="${anime.images.jpg.small_image_url}
+html += `<article class= "card" "js-list-anime" id="${anime.mal_id}">`;
+    //if (anime.url="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"){
+    //  html=""
+   //   html += `<img src="https://via.placeholder.com/210x320/f88ffff/866666/?text=IMG"`}
+//else{
+    html += `<img src="${anime.images.jpg.image_url}";
     alt="img"
     class=" js-img" />`;
-    html += `<p class= "js title">${anime.title}</p>`
+
+    html += `<p class= "title ">${anime.title}</p>`
     html += `</article>`};
 cards.innerHTML = html ;
-callServer();
 }
+
+
+
+
 
 function callServer  (event) {
   event.preventDefault();
@@ -33,17 +44,14 @@ function callServer  (event) {
       result =  json.data;
     });
     renderFilms();
-    callServer();
 };
 search.addEventListener("click", callServer);
 
 
+/*
 
 
-
-
-//if src= "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
-//innerHTML="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
+*/
  
 
 
