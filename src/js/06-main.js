3bf21851a4c2de815ec558670edd876c36a86779
nereazam <nerea_zama@hotmail.com>
2022-07-17 20:12:59 +0200

    add parameters

--- src/js/06-main.js
+++ src/js/06-main.js
@@ -22,65 +22,81 @@ function callServer(event) {
     .then((json) => {
       console.log(json.data);
       result = json.data;
+      localStorage.setItem("data", JSON.stringify(favorites));
+      console.log("hay datos en el local storage");
     });
-  renderFilms();
+  renderFilms(result);
 }
+/*
+function renderFavourites() {
+  let favorite = "";
+  html += `<article class= "card ${classFavorite} js-list-anime" id="${anime.mal_id}">`;
+  if (
+    anime.images.jpg.image_url ===
+    "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+  ) {
+    html += `<img src="https://via.placeholder.com/120x100/f88ffff/866666/?text=IMG" alt="img" class="js-placeholder)/>`;
+  } else {
+    html += `<img src="${anime.images.jpg.image_url}";
+alt="img"
+class=" img js-img" />`;
+  }
+  html += `<p class= "titles js-title">${anime.title}</p>`;
+  html += `</article>`;
 
-function renderFilms() {
+  cards.innerHTML = favorite;
+  listenerSeries();
+}*/
+
+function renderFilms(favorites) {
   let html = "";
   let classFavorite = "";
 
+  //con esto añdo la clase para resaltar el favorito
   for (const anime of result) {
     const favoriteFoundIndex = favorites.findIndex(
-      (fav) => anime.id === fav.id
+      (fav) => anime.mal_id === fav.id
     );
     if (favoriteFoundIndex !== -1) {
       classFavorite = "anime--favorite";
-      left.innerHTML += favorites;
     } else {
       classFavorite = "";
     }
-
+    // con esto pinto cada card
     html += `<article class= "card ${classFavorite} js-list-anime" id="${anime.mal_id}">`;
     if (
       anime.images.jpg.image_url ===
       "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
     ) {
       html += `<img src="https://via.placeholder.com/120x100/f88ffff/866666/?text=IMG" alt="img" class="js-placeholder)/>`;
-      html += `<p class= "titles js-title">${anime.title}</p>;`;
-      html += `</article>`;
     } else {
       html += `<img src="${anime.images.jpg.image_url}";
     alt="img"
     class=" img js-img" />`;
-
-      html += `<p class= "titles js-title">${anime.title}</p>`;
-      html += `</article>`;
     }
+    html += `<p class= "titles js-title">${anime.title}</p>`;
+    html += `</article>`;
 
     cards.innerHTML = html;
   }
 
   listenerSeries();
 }
-
+//funcion que encuentra el click sobr el favorito y lo añade o lo quita del array favorites
 function handelClick(ev) {
   const idSelected = parseInt(ev.currentTarget.id);
-
-  const animeFound = result.find((series) => series.mal_id === idSelected);
-
+  const animeFound = result.find((select) => select.mal_id === idSelected);
   const favoriteFound = favorites.findIndex((fav) => fav.mal_id === idSelected);
-
   if (favoriteFound === -1) {
     favorites.push(animeFound);
   } else {
     favorites.splice(favoriteFound, 1);
   }
-  renderFilms();
+  renderFilms(favorites);
+  listenerSeries();
 }
-
-const listenerSeries = (ev) => {
-  ev.preventDefault();
+//escuchador sobre cada card del array html
+const listenerSeries = () => {
   const liFavorites = document.querySelectorAll(".js-list-anime");
   for (const li of liFavorites) {
     li.addEventListener("click", handelClick);
@@ -101,6 +117,8 @@ reset.addEventListener("click", handelReset);
 function onLocal() {
   const dataLocalStorage = JSON.parse(localStorage.getItem("data"));
   if (dataLocalStorage === null) {
+    favorites = dataLocalStorage;
+    renderFilms(favorites);
   } else {
     callServer();
   }
